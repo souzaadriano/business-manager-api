6e5e107ef0da6641814df1f412b81b224f54302d
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-31 20:58:00 -0300

    feat(main.ts): added redis to start

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { Bootstrap } from './engines/bootstrap';
 import { DatabaseEngine } from './engines/database/database.engine';
 import { EventEngine } from './engines/event/event.engine';
 import { NestEngine } from './engines/nest/nest.engine';
+import { RedisEngine } from './engines/redis/redis.engine';
 
-const application = Bootstrap.createApp([EventEngine, DatabaseEngine, NestEngine]);
+const application = Bootstrap.createApp([EventEngine, DatabaseEngine, RedisEngine, NestEngine]);
 
 application.start().catch(console.error);
